88095b9c9551f7ae4a54a6ffd6a304167ee446fc
Jack O'Connor <oconnor663@gmail.com>
2019-10-14 16:05:14 -0400

    canonicalize fuzzy paths, and strip the cwd on display

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
+use std::env;
+use std::ffi::{OsStr, OsString};
 use std::fs;
 use std::io;
 use std::io::prelude::*;
+use std::path::Path;
 use std::process::{Command, Stdio};
+// Unix-only for now.
+use std::os::unix::ffi::OsStrExt;
 
 fn main() -> io::Result<()> {
     // Create the history dir (~/.local/share/founder or equivalent).
@@ -32,14 +37,20 @@ fn main() -> io::Result<()> {
     let mut fzf_stdout = fzf_handle.stdout.take().unwrap();
 
     // Iterate over history lines backwards from the end. That means that the
-    // most recently appended lines come first. Write each line to fzf. These
-    // will display in the terminal immediately.
+    // most recently appended lines come first. For each line, if the current
+    // working directory is a prefix, strip that off. Then write each line to
+    // fzf. These will display in the terminal immediately.
+    let cwd = env::current_dir()?;
     let mut buf_writer = io::BufWriter::new(&mut fzf_stdin);
     for line in bstr::ByteSlice::rsplit_str(&history_bytes[..], &b"\n"[..]) {
         if line.is_empty() {
             continue;
         }
-        buf_writer.write(line)?;
+        let mut line_path = Path::new(OsStr::from_bytes(line));
+        if line_path.starts_with(&cwd) {
+            line_path = line_path.strip_prefix(&cwd).unwrap();
+        }
+        buf_writer.write(line_path.as_os_str().as_bytes())?;
         buf_writer.write(b"\n")?;
     }
     buf_writer.flush()?;
@@ -60,12 +71,16 @@ fn main() -> io::Result<()> {
     let stripped_selection = &selection[..selection.len() - 1];
     io::stdout().write_all(stripped_selection)?;
 
-    // Add the selection to the history file. Include the \n for this part.
+    // Canonicalize the selection and add that to the history file. Put the
+    // newline back for this part.
+    let mut canonical_path: OsString =
+        fs::canonicalize(OsStr::from_bytes(stripped_selection))?.into();
+    canonical_path.push("\n");
     let mut history_file = fs::OpenOptions::new()
         .append(true)
         .create(true)
         .open(&history_path)?;
-    history_file.write_all(&selection)?;
+    history_file.write_all(canonical_path.as_os_str().as_bytes())?;
 
     // Wait on fzf, then kill fd and wait on that. Fd generally exits promptly,
     // but there's no reason to assume.
