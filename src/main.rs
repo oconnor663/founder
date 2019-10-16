89db520af8e5f3a0be5d9af836ea7857f02a7c70
Jack O'Connor <oconnor663@gmail.com>
2019-10-16 10:12:16 -0400

    compact the history file

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,29 @@
-use anyhow::{Context, Result};
+use anyhow::{anyhow, Context, Result};
 use duct::cmd;
+use once_cell::sync::OnceCell;
 use std::collections::HashSet;
 use std::env;
 use std::ffi::{OsStr, OsString};
 use std::fs;
 use std::io;
 use std::io::prelude::*;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 // Unix-only for now.
 use std::os::unix::ffi::OsStrExt;
 
-fn fd_filter_thread(
+const MAX_HISTORY_LINES: usize = 1000;
+
+fn history_path() -> Result<&'static PathBuf> {
+    static HISTORY_PATH: OnceCell<PathBuf> = OnceCell::new();
+    HISTORY_PATH.get_or_try_init(|| {
+        let user_data_dir = dirs::data_local_dir().ok_or_else(|| anyhow!("no data dir"))?;
+        let founder_dir = user_data_dir.join("founder");
+        fs::create_dir_all(&founder_dir).context("creating history dir")?;
+        Ok(founder_dir.join("history"))
+    })
+}
+
+fn filter_fd_output(
     seen_history: &HashSet<&[u8]>,
     fd_buf_reader: &mut io::BufReader<&duct::ReaderHandle>,
     // By value, so that it's closed implicitly:
@@ -49,6 +62,49 @@ fn fd_filter_thread(
     }
 }
 
+// These lines do not include the terminating newline.
+fn history_lines_from_most_recent(history_bytes: &[u8]) -> impl Iterator<Item = &[u8]> {
+    bstr::ByteSlice::rsplit_str(&history_bytes[..], "\n").filter(|line| !line.is_empty())
+}
+
+fn compact_history_file(history_bytes: &[u8]) -> Result<()> {
+    // Iterate over all the history lines, starting with the most recent, and
+    // collect the first unique occurrence of each one into a vector.
+    let mut lines_set = HashSet::new();
+    let mut ordered_unique_lines = Vec::new();
+    for line in history_lines_from_most_recent(history_bytes) {
+        if lines_set.insert(line) {
+            ordered_unique_lines.push(line);
+        }
+    }
+    // Retain only half the maximum number of lines. (Though pruning duplicates
+    // above might already have brought us below that.) This means that we'll
+    // go a long time between compactions, rather than compacting all the time
+    // when the history file is full of unique entries.
+    ordered_unique_lines.truncate(MAX_HISTORY_LINES / 2);
+    // Write the remaining lines to a temporary file. Once the lines are
+    // written, we'll swap it with the real history file. Note that this
+    // temporary file must be on the same filesystem as the real one, so a
+    // standard temp file in /tmp doesn't work here.
+    let temp_file_path = history_path()?.with_extension("tmp");
+    let temp_file = fs::OpenOptions::new()
+        .write(true)
+        .create_new(true) // error if the file already exists
+        .open(&temp_file_path)?;
+    let mut temp_file_writer = io::BufWriter::new(temp_file);
+    // Note that lines in the history file are oldest-to-newest, which is the
+    // opposite of what's in our vector here, so we reverse it.
+    for line in ordered_unique_lines.iter().rev() {
+        temp_file_writer.write_all(line)?;
+        temp_file_writer.write_all(b"\n")?;
+    }
+    temp_file_writer.flush()?;
+    drop(temp_file_writer);
+    // Swap the new history file into place.
+    fs::rename(&temp_file_path, history_path()?)?;
+    Ok(())
+}
+
 fn main() -> Result<()> {
     // Start the fzf child process with a stdout reader and an explicit stdin
     // pipe. Dropping the reader will implicitly kill fzf, though that will
@@ -65,22 +121,15 @@ fn main() -> Result<()> {
         .context("opening fzf (is fzf installed?)")?;
     let mut fzf_buf_writer = io::BufWriter::new(fzf_stdin_write);
 
-    // Create the history dir (~/.local/share/founder or equivalent).
-    let user_data_dir = dirs::data_local_dir()
-        .ok_or_else(|| io::Error::new(io::ErrorKind::NotFound, "no data dir"))?;
-    let founder_dir = user_data_dir.join("founder");
-    fs::create_dir_all(&founder_dir)?;
-
     // Read all the bytes of the history file. If it doesn't exist, create an
     // empty vec instead.
-    let history_path = founder_dir.join("history");
-    let history_bytes = match fs::read(&history_path) {
+    let history_bytes = match fs::read(history_path()?) {
         Ok(bytes) => bytes,
         Err(e) => {
             if e.kind() == io::ErrorKind::NotFound {
                 Vec::new()
             } else {
-                return Err(e.into());
+                return Err(e).context("reading history");
             }
         }
     };
@@ -92,12 +141,10 @@ fn main() -> Result<()> {
     // set of lines written so far, to suppress duplicates. (Note that this
     // relies on fd not putting ./ at the start of the paths it outputs.)
     let cwd = env::current_dir()?;
-    let mut seen_history = HashSet::new();
-    for line_bytes in bstr::ByteSlice::rsplit_str(&history_bytes[..], &b"\n"[..]) {
-        if line_bytes.is_empty() {
-            continue;
-        }
-        let mut relative_line = Path::new(OsStr::from_bytes(line_bytes));
+    let mut seen_history = HashSet::<&[u8]>::new();
+    let mut history_lines: usize = 0;
+    for line in history_lines_from_most_recent(&history_bytes) {
+        let mut relative_line = Path::new(OsStr::from_bytes(line));
         if relative_line.starts_with(&cwd) {
             relative_line = relative_line.strip_prefix(&cwd).unwrap();
         }
@@ -108,6 +155,7 @@ fn main() -> Result<()> {
         fzf_buf_writer.write(relative_line_bytes)?;
         fzf_buf_writer.write(b"\n")?;
         seen_history.insert(relative_line_bytes);
+        history_lines += 1;
     }
     fzf_buf_writer.flush()?;
 
@@ -123,18 +171,22 @@ fn main() -> Result<()> {
 
     let mut fzf_output = Vec::new();
     crossbeam_utils::thread::scope(|scope| -> Result<()> {
-        // Start the background thread that will manage the fd pipe and
-        // continue writing to the fzf pipe.
-        let fd_thread_handle =
-            scope.spawn(|_| fd_filter_thread(&seen_history, &mut fd_buf_reader, fzf_buf_writer));
+        // Start the background thread that reads the fd pipe and continues
+        // writing to the fzf pipe.
+        scope.spawn(|_| filter_fd_output(&seen_history, &mut fd_buf_reader, fzf_buf_writer));
+
+        // If the history file is at capacity, start another background thread
+        // to compact it. Leaving this scope will join this thread.
+        if history_lines >= MAX_HISTORY_LINES {
+            scope.spawn(|_| compact_history_file(&history_bytes));
+        }
 
         // Read the selection from fzf. (Fzf might return an error, but we'll
         // check that below.)
         (&fzf_reader).read_to_end(&mut fzf_output)?;
 
-        // Kill fd and explicitly join the fd thread.
+        // Kill fd. We'll explicitly join that thread as we leave this scope.
         fd_reader.kill()?;
-        fd_thread_handle.join().unwrap()?;
 
         Ok(())
     })
@@ -150,6 +202,7 @@ fn main() -> Result<()> {
     assert_eq!(fzf_output[fzf_output.len() - 1], '\n' as u8);
     let stripped_selection = &fzf_output[..fzf_output.len() - 1];
     io::stdout().write_all(stripped_selection)?;
+    io::stdout().flush()?;
 
     // Canonicalize the selection and add that to the history file. Put the
     // newline back for this part.
@@ -159,7 +212,7 @@ fn main() -> Result<()> {
     let mut history_file = fs::OpenOptions::new()
         .append(true)
         .create(true)
-        .open(&history_path)?;
+        .open(history_path()?)?;
     history_file.write_all(canonical_path.as_os_str().as_bytes())?;
 
     Ok(())
